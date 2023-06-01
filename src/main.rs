f03bc48cd8b058bfe58c95f410e4abd211ff9511
Aryan <52215513+importly@users.noreply.github.com>
2023-05-31 22:01:21 -0400

    set up basic cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use structopt::{clap::arg_enum, StructOpt};
+
+arg_enum! {
+    #[derive(Debug)]
+    enum App {
+        Svelte,
+        React,
+        Vue,
+        Angular,
+    }
+}
+
+arg_enum! {
+    #[derive(Debug)]
+    enum Runtime {
+        Flask,
+        Express,
+        Django,
+        Actix,
+    }
+}
+
+#[derive(StructOpt, Debug)]
+enum Cli {
+    Deploy {
+        #[structopt(short, long, possible_values = &App::variants(), case_insensitive = true)]
+        app: App,
+
+        #[structopt(short, long, possible_values = &Runtime::variants(), case_insensitive = true)]
+        runtime: Runtime,
+
+        #[structopt(short="t",long)]
+        region: String,
+
+        #[structopt(short, long)]
+        name: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{:?}", args);
+
+    match args {
+        Cli::Deploy { app, runtime, region, name } => {
+            println!("Deploying {} {} app to {} with name {}", app, runtime, region, name);
+        }
+    }
+
 }
